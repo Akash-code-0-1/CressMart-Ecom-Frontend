cfb4fc78a68293f8ae2f8a1fd84b89e4cfde680b
Tanvir Ahmed Akash <mdtanvirahmedakash.1@gmail.com>
2026-07-09 06:51:53 +0600

    fix for deployment

--- src/app/admin/(dashboard)/dashboard/products/page.tsx
+++ src/app/admin/(dashboard)/dashboard/products/page.tsx
@@ -1,14 +1,39 @@
+// "use client";
+// import ProductTable from "@/components/admin/products/ProductTable";
+// import ProductToolbar from "@/components/admin/products/ProductToolbar";
+
+// export default function Page() {
+//   return (
+//     <div className="flex overflow-hidden">
+//       <main className="w-full">
+//         <div className="p-2 md:p-0 mt-3">
+//           <ProductToolbar />
+//           <ProductTable />
+//         </div>
+//       </main>
+//     </div>
+//   );
+// }
+
+
 "use client";
-import ProductTable from "@/components/admin/products/ProductTable";
+
+import { Suspense } from "react";
 import ProductToolbar from "@/components/admin/products/ProductToolbar";
+import ProductTable from "@/components/admin/products/ProductTable";
 
 export default function Page() {
   return (
     <div className="flex overflow-hidden">
       <main className="w-full">
         <div className="p-2 md:p-0 mt-3">
-          <ProductToolbar />
-          <ProductTable />
+          <Suspense fallback={null}>
+            <ProductToolbar />
+          </Suspense>
+
+          <Suspense fallback={null}>
+            <ProductTable />
+          </Suspense>
         </div>
       </main>
     </div>
